3cb3920fe88cc4feeaad5fa4938b39cfd00e4bd2
Joonas P <jopotthast@gmail.com>
2019-12-10 13:33:08 +0100

    Ablauf Klasse

---
+++ AB_09/Ablauf.java
@@ -0,0 +1,6 @@
+public class Ablauf {
+    public static void main(String[] args) {
+        Angestellter meyer = new Angestellter(4560, 2000, 2006, 1234);
+        meyer.ausgeben();
+    }
+}
--- AB_09/Angestellter.java
+++ AB_09/Angestellter.java
@@ -7,4 +7,15 @@ public class Angestellter extends Arbeitnehmer{
         super(jahr, pnr);
         this.bruttoGehalt = brutto;
     }
+
+    public double ermittleNetto(){
+        return this.bruttoGehalt - this.abzuege;
+    }
+
+    public void ausgeben(){
+        System.out.println("Bruttogehalt: "+this.bruttoGehalt);
+        System.out.println("Abzüge: "+this.abzuege);
+        System.out.println("Nettogehalt: "+this.ermittleNetto());
+        super.anzeigen();
+    }
 }
